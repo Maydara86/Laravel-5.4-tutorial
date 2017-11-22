bb198e3c63251a968a0c14a6162dd27025b0dd81
Mohamed Cherif <cherif.mohamed86@gmail.com>
2017-11-23 00:28:04 +0100

    Eloquent 101

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,23 @@
 |
 */
 
+use App\Task;
+
 Route::get('/', function () {
-    $tasks = DB::table('tasks')->get();
+    $tasks = DB::table('tasks')->get();    
     
     return view('welcome', compact('tasks'));
 });
 
 Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
+    $tasks = Task::all();
     
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-    $task = DB::table('tasks')->find($id);
-    
+    $task = Task::find($id);
+
     return view('tasks/show', compact('task'));
 });
 
