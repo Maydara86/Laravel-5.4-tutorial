3f411b3cdba76153fe1002d79e45c95ae899a371
Mohamed Cherif <cherif.mohamed86@gmail.com>
2017-12-11 15:47:04 +0100

    Ep15 Eloquent Relationships and Comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,4 +10,20 @@
         {{$post->body}}
     </div>
 
+    <hr>
+
+    <div class="comments">
+        <ul class="list-group">
+
+        @foreach($post->comments as $comment)
+
+        <li class="list-group-item">
+        <strong>{{$comment->created_at->diffForHumans()}}:</strong> {{$comment->body}}</li>
+
+        @endforeach    
+        
+        </ul>
+    </div>
+    
+
 @endsection
